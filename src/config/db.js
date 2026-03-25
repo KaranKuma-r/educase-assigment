89d35eee33d44fd05659184d9c0f6075a30ab179
Karan Kumar <kk5214662@gmail.com>
2026-03-25 14:02:59 +0530

    add:MYSQL_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,13 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
     console.log("DB Error:", err);
   } else {
-    console.log("MySQL Connected");
+    console.log("MySQL Connected ✅");
   }
 });
 
